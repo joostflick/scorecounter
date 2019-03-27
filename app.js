4bae2dfeb22dff661c9b599ec7c8bb2870a35f15
Joost Flick <joostflick@gmail.com>
2019-03-27 13:08:15 +0100

    offline/online functionality

--- app.js
+++ app.js
@@ -4,9 +4,6 @@ var path = require('path')
 var cookieParser = require('cookie-parser')
 var logger = require('morgan')
 
-var indexRouter = require('./routes/index')
-var usersRouter = require('./routes/users')
-
 var app = express()
 
 // view engine setup
@@ -19,59 +16,65 @@ app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
-// app.use('/', res.render('index'))
-app.use('/users', usersRouter)
-
 let team1Points = 0
 let team2Points = 0
 
+app.get('/update'),
+  (req, res) => {
+    console.log(req)
+    res.render('index', { team1Points: team1Points, team2Points: team2Points })
+  }
+
 app.get('/', (req, res) => {
-	res.render('index', { team1Points: team1Points, team2Points: team2Points })
+  if (req.query.scoreTeam1) {
+    team1Points = req.query.scoreTeam1
+    team2Points = req.query.scoreTeam2
+  } else {
+    console.log('No offline data retrieved')
+  }
+
+  res.render('index', { team1Points: team1Points, team2Points: team2Points })
 })
 
 app.post('/plus-team1', (req, res) => {
-	team1Points++
-	console.log(team1Points)
-	res.render('index', { team1Points: team1Points, team2Points: team2Points })
+  team1Points++
+  res.render('index', { team1Points: team1Points, team2Points: team2Points })
 })
 app.post('/minus-team1', (req, res) => {
-	if (team1Points > 0) {
-		team1Points--
-	} else {
-		team1Points = team1Points
-	}
-	console.log('team 1 minus 1 point')
-	res.render('index', { team1Points: team1Points, team2Points: team2Points })
+  if (team1Points > 0) {
+    team1Points--
+  } else {
+    team1Points = team1Points
+  }
+  res.render('index', { team1Points: team1Points, team2Points: team2Points })
 })
 app.post('/plus-team2', (req, res) => {
-	team2Points++
-	console.log(team2Points)
-	res.render('index', { team2Points: team2Points, team1Points: team1Points })
+  team2Points++
+  res.render('index', { team2Points: team2Points, team1Points: team1Points })
 })
 app.post('/minus-team2', (req, res) => {
-	if (team2Points > 0) {
-		team2Points--
-	} else {
-		team2Points = team2Points
-	}
-	console.log(team2Points)
-	res.render('index', { team2Points: team2Points, team1Points: team1Points })
+  if (team2Points > 0) {
+    team2Points--
+  } else {
+    team2Points = team2Points
+  }
+  res.render('index', { team2Points: team2Points, team1Points: team1Points })
 })
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-	next(createError(404))
+  next(createError(404))
 })
 
 // error handler
 app.use(function(err, req, res, next) {
-	// set locals, only providing error in development
-	res.locals.message = err.message
-	res.locals.error = req.app.get('env') === 'development' ? err : {}
+  // set locals, only providing error in development
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
 
-	// render the error page
-	res.status(err.status || 500)
-	res.render('error')
+  // render the error page
+  res.status(err.status || 500)
+  res.render('error')
 })
 
 module.exports = app
